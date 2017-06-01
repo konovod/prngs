b0509a96ec93af587dabb7b39d38f4b020051c9f
Konovod <konovod@gmail.com>
2017-06-01 09:14:00 +0000

    fixed benchmark

--- bench.cr
+++ bench.cr
@@ -8,10 +8,13 @@ gens = {
   Random::Xorshift1024star.new,
   Random::Xoroshiro128plus.new,
   Random::PCG32.new,
+  Random::WELL512.new,
 }
 
+x = 0 # to ensure that optimizations won't remove rand calls
 Benchmark.ips do |bench|
   gens.each do |gen|
-    bench.report(gen.class.to_s) { gen.rand }
+    bench.report(gen.class.to_s) { x += gen.rand }
   end
 end
+p x
