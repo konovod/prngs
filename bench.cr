7797df541df0966a315a2e5ff40c7c0228ca5738
Konovod <konovod@gmail.com>
2017-06-08 19:45:37 +0000

    added SecureRandom to benchmark

--- bench.cr
+++ bench.cr
@@ -1,4 +1,5 @@
 require "random/isaac"
+require "secure_random"
 require "benchmark"
 require "./src/prngs"
 
@@ -11,10 +12,12 @@ gens = {
   Random::WELL512.new,
 }
 
-x = 0 # to ensure that optimizations won't remove rand calls
+x = 0.0 # to ensure that optimizations won't remove rand calls
+bytes = Bytes.new(4)
 Benchmark.ips do |bench|
   gens.each do |gen|
     bench.report(gen.class.to_s) { x += gen.rand }
   end
+  bench.report("SecureRandom") {SecureRandom.random_bytes(bytes)}
 end
 p x
